dd34a74bcfe50e232813439750e799f4a2639aa1
anima-boy <69791587+anima-boy@users.noreply.github.com>
2020-08-20 16:50:43 +0300

    homework 2.1

--- main.js
+++ main.js
@@ -24,3 +24,11 @@ function getRow(firstRow, secondRow) {
 }
 
 console.log(getRow(firstRow, secondRow)); // мама мыла раму
+
+// второе
+
+function formattedPhone(phone) {
+    return phone.charAt(0) + phone.charAt(1) + ' ' +'('+phone.charAt(2)+phone.charAt(3) +phone.charAt(4) + ')'+' '+phone.charAt(5) + phone.charAt(6) + phone.charAt(7) + '-' + phone.charAt(8) + phone.charAt(9)+'-'+phone.charAt(10)+phone.charAt(11)
+}
+
+console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
